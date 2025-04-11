468b5e566cfa1c4ec4a0f53b5691da41b1200faf
Lucca Giordano <luccagiordano05@gmail.com>
2025-04-10 22:13:21 -0300

    Servidor atualizado
    Começo da tela de login

--- src/FrontEnd/src/App.jsx
+++ src/FrontEnd/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import LandingPage from "./LandingPage";
+import LoginPage from "./LoginPage";
+
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
